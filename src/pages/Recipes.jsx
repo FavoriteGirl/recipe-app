a64d6f06295cb629e1771941e342ce8f8d1147f3
FavoriteGirl <jayicon6@gmail.com>
2024-06-28 09:29:21 +0000

    React Router

---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// const Recipes = () => {
+//   return (
+//     <div>Recipes</div>
+//   )
+// };
+
+// export default Recipes;
+
+export default function Recipes(){
+    return(
+        <div>Recipes</div>
+    );
+}
